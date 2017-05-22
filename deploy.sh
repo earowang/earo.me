a558a3a2ae3212e7627d7bd46f73be8729945671
earowang <earo.wang@gmail.com>
2017-05-22 11:37:56 +1000

    update medascin17 slides

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "\033[0;32mDeploying updates to earo.me...\033[0m"
 
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
